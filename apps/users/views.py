2544bdb11c5f9dd8d4856cfdafdf28a4d6b652bb
Ulan Isazhan <u.isazhan@mail.ru>
2023-01-11 20:12:58 +0600

    login function has been added

---
+++ apps/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'users/login.html', {'title': 'Страница входа'})
+    else:
+        return render(request, 'users/login.html', {'title': 'Страница входа'})
--- psi/settings.py
+++ psi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.main',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login'
+LOGOUT_URL = '/login'
