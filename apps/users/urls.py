e4c1cb8def6771f5fc968e29a7e6568ddb185be9
Ulan Isazhan <u.isazhan@mail.ru>
2023-01-17 14:39:56 +0600

    create profile page

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
+    path('profile/', views.profile, name='profile'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, models
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def login_user(request):
@@ -19,3 +22,15 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return render(request, 'users/login.html', {'title': 'Страница входа'})
+
+
+@login_required
+def profile(request):
+    users = models.User.objects.get(username=request.user)
+    context = {
+        'firstname': users.first_name,
+        'lastname': users.last_name,
+        'email': users.email,
+    }
+    template = loader.get_template('users/profile.html')
+    return HttpResponse(template.render(context, request))
