bc84988e99d68d2c1aeb7e15c6afdf443dd77572
Ulan Isazhan <u.isazhan@mail.ru>
2024-05-24 21:12:09 +0500

    equipment list start

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from db import get_db_handle as db
+from django.shortcuts import render, redirect
+
 
 @login_required
 def index(request):
@@ -22,6 +22,7 @@ def index(request):
 
     return HttpResponse(template.render(context, request))
 
+
 @login_required
 def project(request, project_number):
 
@@ -31,3 +32,27 @@ def project(request, project_number):
     
     template = loader.get_template('main/project.html')
     return HttpResponse(template.render(context, request))
+
+
+@login_required
+def equipment_list(request, project_number):
+    if request.method == 'POST':
+        col = db()[project_number+'equipment_list']
+        x = col.insert_one(request.POST.dict())
+        return redirect('equipment_list', project_number)
+    else:
+        col = db()[project_number+'equipment_list']
+        doc = col.find({})
+
+        context = {
+            'project_number': project_number,
+            'equipment_list': doc,
+        }
+        
+        template = loader.get_template('main/equipment_list.html')
+        return HttpResponse(template.render(context, request))
+
+
+@login_required
+def equipment(request, project_number, equipment_tag):
+    return
